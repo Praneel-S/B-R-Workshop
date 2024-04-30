265d0cf6db9cad1d503092041d00287bc2c31e85
Praneel-S <145468353+Praneel-S@users.noreply.github.com>
2024-04-30 14:50:24 +0530

    Update QR.py

--- QR.py
+++ QR.py
@@ -1,19 +1,21 @@
 import qrcode
 
-def generate_qr_code(text, filename='qrcode.png'):
+def generate_qr_code(data, filename):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data(text)
+    qr.add_data(data)
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(filename)
-    print(f"QR code saved as {filename}")
 
-# Example usage:
-text_to_encode = "Area 51 is fake but aliens are real!"
-generate_qr_code(text_to_encode)
+if __name__ == "__main__":
+    # Example data for QR code
+    data = "Area 51 is fake. Aliens are real and they are called test tube babies."
+
+    # Generate QR code image
+    generate_qr_code(data, "qrcode.png")
